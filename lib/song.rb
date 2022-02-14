60ca4a1a8ef6c974caaa24c9493c7f648366d032
MD <dayclawtel@gmail.com>
2022-02-14 13:03:36 -0800

    commit tree 0e1b66caa643c7477cc25e1cf1f3d41cda6b6b3c

--- lib/song.rb
+++ lib/song.rb
@@ -30,15 +30,16 @@ class Song
     end
 
     def self.genre_count
-        genre_count = {}
-        @@genres.each do |genre|
-            if genre_count[genre]
-                genre_count[genre] += 1
-            else
-                genre_count[genre] = 1
-            end
-        end
-        genre_count
+        # genre_count = {}
+        # @@genres.each do |genre|
+        #     if genre_count[genre]
+        #         genre_count[genre] += 1
+        #     else
+        #         genre_count[genre] = 1
+        #     end
+        # end
+        # genre_count
+        @@genres.tally
     end
 
     def self.artist_count
